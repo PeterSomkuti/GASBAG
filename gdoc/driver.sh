a8cba1970fa363adcf88f3b0c852f58286345e7b
Heather Cronk <heather.cronk@colostate.edu>
2019-11-27 14:32:15 -0700

    Let it run

--- gdoc/driver.sh
+++ gdoc/driver.sh
@@ -8,5 +8,5 @@ for control_file in ${control_files[@]}
 do
     echo "Processing" $control_file
     OMP_NUM_THREADS=4 /GASBAG/build/GASBAG -c ${control_file_dir}/${control_file}
-    exit
+    #exit
 done # control file loop

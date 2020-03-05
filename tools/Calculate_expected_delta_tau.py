Fixed error in pressure weight calculation.

tools/Calculate_expected_delta_tau.py
@@ -3,8 +3,8 @@ import h5py
 
 
 # Clear-scene radiance
-f = h5py.File('/data8/ttaylor/data_ttaylor/sim_archive/OCO3_orbit_simulations/r93_g04/clear_fluor-off_surf-MSCM_noise-off/intensity/2012_01/OCO3_sim_r93_intensity_clear_fluor-off_surf-MSCM_noise-off_2012_01.hdf', 'r')
-
+#f = h5py.File('/data8/ttaylor/data_ttaylor/sim_archive/OCO3_orbit_simulations/r93_g04/clear_fluor-off_surf-MSCM_noise-off/intensity/2012_01/OCO3_sim_r93_intensity_clear_fluor-off_surf-MSCM_noise-off_2012_01.hdf', 'r')
+f = h5py.File('/home/gregm/geocarb_testing/data/radiative_transfer/output-20160321_20x10_065w-no_aerosol-brdf_3-with_noise/geocarb_l1b_rx_intensity_20160321_20x10_065w-no_aerosol-brdf_3-with_noise.hdf', 'r')
 
 N = f['SoundingGeometry/sounding_zenith'].shape[0]
 SZA = f['SoundingGeometry/sounding_solar_zenith'][:,0]
@@ -67,4 +67,37 @@ for j in range(strong_delta_tau.shape[1]):
     print("------STRONG CO2--------")
     print("Wavelength in and out: ", strong_wl_in[j], strong_wl_out[j])
     print("Mean (std) delta tau: {:.2f} ({:.2f})"
-          .format(strong_delta_tau[:,j].mean(), strong_delta_tau[:,j].std()))
+          .format(np.nanmean(strong_delta_tau[:,j]),
+                  np.nanstd(strong_delta_tau[:,j]))
+          )
+
+ch4_pixels = np.arange(f['SoundingMeasurements/radiance_ch4'].shape[2]) + 1
+ch4_wavelength = np.poly1d(f['InstrumentHeader/dispersion_coef_samp'][3,0][::-1])(ch4_pixels)
+
+ch4_wl_in = [2.32194, 2.345, 2.317]
+ch4_wl_out = [2.32233, 2.3442, 2.314]
+ch4_delta_tau = np.zeros((N, len(ch4_wl_in)))
+ch4_idx_in = np.zeros(len(ch4_wl_in), dtype='int')
+ch4_idx_out = np.zeros(len(ch4_wl_in), dtype='int')
+
+for j in range(ch4_delta_tau.shape[1]):
+    ch4_idx_in[j] = np.argmin(np.abs(ch4_wavelength - ch4_wl_in[j]))
+    ch4_idx_out[j] = np.argmin(np.abs(ch4_wavelength - ch4_wl_out[j]))
+
+
+for j in range(ch4_delta_tau.shape[1]):
+    log_intensity = np.log(
+        f['SoundingMeasurements/radiance_ch4'][:, 0, ch4_idx_out[j]] /
+        f['SoundingMeasurements/radiance_ch4'][:, 0, ch4_idx_in[j]]
+            )
+
+    ch4_delta_tau[:,j] = log_intensity / mu_bar
+
+# Print it all out
+for j in range(ch4_delta_tau.shape[1]):
+    print("------ CH4 --------")
+    print("Wavelength in and out: ", ch4_wl_in[j], ch4_wl_out[j])
+    print("Mean (std) delta tau: {:.2f} ({:.2f})"
+          .format(np.nanmean(ch4_delta_tau[:,j]),
+                  np.nanstd(ch4_delta_tau[:,j]))
+          )
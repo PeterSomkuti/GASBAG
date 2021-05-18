915835bfbad56ccdc835f5cd729b7945f93c582e
Peter Somkuti <psomkuti@colostate.edu>
2021-05-18 15:37:18 -0600

    Compatibility with gfortran 10 and up. Put perform_retrievals into a module.

--- src/retrieval/perform_retrievals.f90
+++ src/retrieval/perform_retrievals.f90
@@ -7,69 +7,76 @@
 !> including setting them up. All necessary information about the settings etc.,
 !> is avaliable through the MCS module.
 !>
-!> @param my_instrument Instrument entity
-subroutine perform_retrievals(my_instrument, CS)
 
-  ! User modules
-  use control_mod, only: CS_t
-  use instruments_mod, only: generic_instrument
-  use file_utils_mod, only: get_HDF5_dset_dims, check_hdf_error
-  use guanter_model_mod
-  use physical_model_mod
-  use oco2_mod
+module perform_retrievals_mod
 
-  ! Third-party modules
+contains
 
-  ! System modules
-  use HDF5
+  !> @param my_instrument Instrument entity
+  subroutine perform_retrievals(my_instrument, CS)
 
-  implicit none
+    ! User modules
+    use control_mod, only: CS_t
+    use instruments_mod, only: generic_instrument
+    use file_utils_mod, only: get_HDF5_dset_dims, check_hdf_error
+    use guanter_model_mod
+    use physical_model_mod
+    use oco2_mod
 
-  class(generic_instrument) :: my_instrument
-  type(CS_t), intent(inout) :: CS
+    ! Third-party modules
 
-  ! Local variables
-  character(len=*), parameter :: fname = "perform_retrieval" ! Function name for logging
-  integer(hid_t) :: l1b_file_id ! L1b HDF file handler
-  logical :: SoundingGeometry_exists ! Does the SoundingGeometry group exist?
-  integer :: hdferr ! HDF error variable
+    ! System modules
+    use HDF5
 
+    implicit none
 
-  ! Open the L1B_HDF file - this will generally be required
-  call h5fopen_f(CS%input%L1B_filename%chars(), H5F_ACC_RDONLY_F, l1b_file_id, hdferr)
-  call check_hdf_error(hdferr, fname, "Error opening L1B HDF file: " // trim(CS%input%L1B_filename%chars()))
+    class(generic_instrument) :: my_instrument
+    type(CS_t), intent(inout) :: CS
 
-  ! .. and store the file ID in the MCS
-  CS%input%l1b_file_id = l1b_file_id
-
-  select type(my_instrument)
-  type is (oco2_instrument)
-     ! We copy the SoundingGeometry group over to the results section, for
-     ! easy analysis of the results later on. (This is a fairly small amount of data)
-
-     call h5lexists_f(l1b_file_id, "/SoundingGeometry", SoundingGeometry_exists, hdferr)
-     if (SoundingGeometry_exists) then
-        call h5ocopy_f(l1b_file_id, "/SoundingGeometry", &
-             CS%output%output_file_id, "/SoundingGeometry", hdferr)
-        call check_hdf_error(hdferr, fname, "Error copying /SoundingGeometry into output file")
-     else
-        call logger%debug(fname, "/SoundingGeometry not part of this L1B file. Skipping.")
-     end if
-  end select
+    ! Local variables
+    character(len=*), parameter :: fname = "perform_retrieval" ! Function name for logging
+    integer(hid_t) :: l1b_file_id ! L1b HDF file handler
+    logical :: SoundingGeometry_exists ! Does the SoundingGeometry group exist?
+    integer :: hdferr ! HDF error variable
 
 
-  if (CS%algorithm%using_GK_SIF) then
-     ! Launch Guanter Retrievals
-     call guanter_retrieval(my_instrument, CS)
-  end if
+    ! Open the L1B_HDF file - this will generally be required
+    call h5fopen_f(CS%input%L1B_filename%chars(), H5F_ACC_RDONLY_F, l1b_file_id, hdferr)
+    call check_hdf_error(hdferr, fname, "Error opening L1B HDF file: " // trim(CS%input%L1B_filename%chars()))
 
-  if (CS%algorithm%using_physical) then
-     ! Launch physical-type retrievals
-     call physical_retrieval(my_instrument, CS)
-  end if
-  
-  ! Close L1B HDF file after we're done.
-  call h5fclose_f(CS%input%l1b_file_id, hdferr)
-  call check_hdf_error(hdferr, "Main", "Error closing input HDF5 file")
+    ! .. and store the file ID in the MCS
+    CS%input%l1b_file_id = l1b_file_id
 
-end subroutine perform_retrievals
+    select type(my_instrument)
+    type is (oco2_instrument)
+       ! We copy the SoundingGeometry group over to the results section, for
+       ! easy analysis of the results later on. (This is a fairly small amount of data)
+
+       call h5lexists_f(l1b_file_id, "/SoundingGeometry", SoundingGeometry_exists, hdferr)
+       if (SoundingGeometry_exists) then
+          call h5ocopy_f(l1b_file_id, "/SoundingGeometry", &
+               CS%output%output_file_id, "/SoundingGeometry", hdferr)
+          call check_hdf_error(hdferr, fname, "Error copying /SoundingGeometry into output file")
+       else
+          call logger%debug(fname, "/SoundingGeometry not part of this L1B file. Skipping.")
+       end if
+    end select
+
+
+    if (CS%algorithm%using_GK_SIF) then
+       ! Launch Guanter Retrievals
+       call guanter_retrieval(my_instrument, CS)
+    end if
+
+    if (CS%algorithm%using_physical) then
+       ! Launch physical-type retrievals
+       call physical_retrieval(my_instrument, CS)
+    end if
+
+    ! Close L1B HDF file after we're done.
+    call h5fclose_f(CS%input%l1b_file_id, hdferr)
+    call check_hdf_error(hdferr, "Main", "Error closing input HDF5 file")
+
+  end subroutine perform_retrievals
+
+end module perform_retrievals_mod

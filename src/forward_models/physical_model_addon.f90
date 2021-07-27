b27b4bb8308029f20068e232feb6731a2181ee68
Peter Somkuti <psomkuti@colostate.edu>
2021-07-27 09:56:45 -0600

    Removed some debug stuff.

--- src/forward_models/physical_model_addon.f90
+++ src/forward_models/physical_model_addon.f90
@@ -242,25 +242,6 @@ contains
 
     end do ! Layer loop
 
-    if (any(ieee_is_nan(coef))) then
-       call logger%error(fname, "I found NaN(s) in the COEF calculation.")
-
-       read(*,*)
-       do a = 1, size(coef, 1)
-          do l = 1, size(coef, 2)
-             do p = 1, size(coef, 3)
-
-                if (ieee_is_nan(coef(a,l,p))) then
-                   write(*,*) "NaN at: ",  a, l, p
-                end if
-             end do
-          end do
-       end do
-
-       read(*,*)
-       return
-    end if
-
     if (any(ieee_is_nan(lcoef))) then
        call logger%error(fname, "I found NaN(s) in the LCOEF calculation.")
        return
